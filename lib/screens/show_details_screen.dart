0471e5050d943c0403ba697da2eaa3d5af6b4b25
GouravShDev <gourav0sharma1@gmail.com>
2021-03-05 21:50:21 +0530

    Started working on show_details_screen

--- lib/screens/show_details_screen.dart
+++ lib/screens/show_details_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:tv_maze/models/shows.dart';
 import 'package:tv_maze/services/api_services.dart';
@@ -17,9 +18,10 @@ class ShowDetails extends StatefulWidget {
 class _ShowDetailsState extends State<ShowDetails> {
   Shows _show;
   _initShow() async{
-    var show = await ApiService.instance.fetchShow(id: widget.id);
-    setState(() {
-      _show = show;
+    await ApiService.instance.fetchShow(id: widget.id).then((show) {
+      setState(() {
+        _show = show;
+      });
     });
   }
 
@@ -31,30 +33,90 @@ class _ShowDetailsState extends State<ShowDetails> {
   }
   @override
   Widget build(BuildContext context) {
-    // final mediaQuery = MediaQuery.of(context);
+    final mediaQuery = MediaQuery.of(context);
+    final height = mediaQuery.size.height;
+    final width = mediaQuery.size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       // Added Center widget as placeholder for the time being
-      body: 1==1 ? Center(child: Text("Work in Progress"),):Column(
-        children: [
-          Row(
-            children: [
-              Image.network(_show.imageUrl),
-              Column(
-                children: [
-                  Text('Show Info'),
-                  Text('genres : ${_show.genres}'),
-                  Text('language : ${_show.language}'),
-                  Text('Premiered On : ${_show.premieredOn}'),
-                ],
-              )
-            ],
-          ),
-          Text(_show.summary),
-        ],
-      ),
+      body: _show == null
+          ? Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(
+                  Theme.of(context).primaryColor, // Red
+                ),
+              ),
+            )
+          : Column(
+              children: [
+                Container(
+                  // margin: EdgeInsets.all(10),
+                  height: 300,
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Image.network(
+                        _show.imageUrl,
+                        fit: BoxFit.fitWidth,
+                      ),
+                      ClipRRect(
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                          child: Container(
+                            color: Colors.grey.withOpacity(0.1),
+                            alignment: Alignment.center,
+                            child: Container(
+                                padding: EdgeInsets.only(
+                                  top: mediaQuery.padding.top * 0.8,
+                                ),
+                                child: Image.network(
+                                  _show.imageUrl,
+                                  height: 250,
+                                )),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                          top: mediaQuery.padding.top * 0.8,
+                          left: 0,
+                          child: Container(
+                            decoration: ShapeDecoration(
+                              color: Theme.of(context).canvasColor,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.only(
+                                      bottomRight: Radius.circular(width * .1),
+                                      topRight: Radius.circular(width * .1),),),
+                            ),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.arrow_back_outlined,
+                                size: height * 0.035,
+                                color:
+                                    Theme.of(context).textTheme.headline6.color,
+                              ),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          )),
+                      Positioned(bottom: 10, child: Text(_show.name)),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Text('Show Info'),
+                        Text('genres : ${_show.genres}'),
+                        Text('language : ${_show.language}'),
+                        Text('Premiered On : ${_show.premieredOn}'),
+                      ],
+                    )
+                  ],
+                ),
+                Text(_show.summary),
+              ],
+            ),
     );
   }
 }
